05acc1a7a9056d89907679df8939133a92e8f720
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-01-14 14:58:36 -0300

    concluida aula Map

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-const list = document.querySelector('#show-items')
-const buttonShowAll = document.querySelector(".show-all")
+const tagList = document.querySelector("#show-items");
+const buttonShowAll = document.querySelector(".show-all");
+const buttonMapAll = document.querySelector(".map-all");
 
-let myLi = ''
-
-function showAll() {
-    menuOptions.forEach((product) => {
-        myLi +=
-            `
-         <li>
-            <img src=${product.src}>
-            <p>${product.name}</p>
-            <p class="item-price">R$ ${product.price}</p>
-        </li>
-         `
+function ShowAll(productsArray) {
+    let myLi = ``;
+    productsArray.forEach((product) => {
+        myLi += `
+        <li>
+                <img src=${product.src}>
+                <p>${product.name}</p>
+                <p class="item-price">R$ ${product.price}</p>
+            </li>
+        `;
     })
-    console.log(myLi)
-
-list.innerHTML = myLi
+    tagList.innerHTML = myLi;
 }
 
+function mapAllItems() {
+    const newPrices = menuOptions.map((product) => ({
+        ...product,
+        price: product.price * 0.9
+    }))
+    ShowAll(newPrices);
+}
 
-
-
-buttonShowAll.addEventListener("click", showAll)
+buttonShowAll.addEventListener("click", () => ShowAll(menuOptions));
+buttonMapAll.addEventListener("click", mapAllItems);
