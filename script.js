3daac16cb044aa8ff0a7a45170d92aec248ae5ab
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-01-14 16:39:26 -0300

    concluida aula Reduce

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
 const tagList = document.querySelector("#show-items");
+const buttonSumAll = document.querySelector(".sum-all");
 const buttonShowAll = document.querySelector(".show-all");
 const buttonMapAll = document.querySelector(".map-all");
 
-function ShowAll(productsArray) {
-    let myLi = ``;
-    productsArray.forEach((product) => {
-        myLi += `
-        <li>
-                <img src=${product.src}>
-                <p>${product.name}</p>
-                <p class="item-price">R$ ${product.price}</p>
-            </li>
-        `;
-    })
-    tagList.innerHTML = myLi;
+
+
+function showAll(arrayList) {
+    let myList = ``;
+    arrayList.forEach(product => {
+        myList +=
+            `
+    <li>
+            <img src=${product.src}>
+            <p>${product.name}</p>
+            <p class="item-price">R$ ${product.price}</p>
+        </li>
+    `
+    });
+    tagList.innerHTML = myList;
 }
 
-function mapAllItems() {
-    const newPrices = menuOptions.map((product) => ({
+function mapAll() {
+    const newPrice = menuOptions.map(product => ({
         ...product,
         price: product.price * 0.9
     }))
-    ShowAll(newPrices);
+    showAll(newPrice);
 }
 
-buttonShowAll.addEventListener("click", () => ShowAll(menuOptions));
-buttonMapAll.addEventListener("click", mapAllItems);
+function sumAllItens() {
+    const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0);
+
+    tagList.innerHTML =
+        `
+    <li>
+     <p>O valor  total dos itens é R$ ${totalValue}</pass=>
+    </li>
+   
+    `;
+}
+
+
+buttonShowAll.addEventListener("click", () => showAll(menuOptions))
+buttonMapAll.addEventListener("click", mapAll)
+buttonSumAll.addEventListener("click", sumAllItens)
