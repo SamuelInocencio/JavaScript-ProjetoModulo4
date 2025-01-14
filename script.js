bf38d2e796795e7cca3f569498e85dcfc322d6c0
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-01-14 18:27:36 -0300

    concluido o projeto

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const tagList = document.querySelector("#show-items");
 const buttonSumAll = document.querySelector(".sum-all");
 const buttonShowAll = document.querySelector(".show-all");
 const buttonMapAll = document.querySelector(".map-all");
+const buttonFilterAll = document.querySelector(".filter-all");
 
-
+function formatCurrency(currency) {
+    return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(currency);
+}
 
 function showAll(arrayList) {
     let myList = ``;
@@ -13,7 +19,7 @@ function showAll(arrayList) {
     <li>
             <img src=${product.src}>
             <p>${product.name}</p>
-            <p class="item-price">R$ ${product.price}</p>
+            <p class="item-price">${formatCurrency(product.price)}</p>
         </li>
     `
     });
@@ -29,18 +35,22 @@ function mapAll() {
 }
 
 function sumAllItens() {
-    const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0);
-
+    const sumItens = menuOptions.reduce((acc, curr) => acc + curr.price, 0);
     tagList.innerHTML =
         `
     <li>
-     <p>O valor  total dos itens é R$ ${totalValue}</pass=>
-    </li>
-   
+         <p>O valor total é ${formatCurrency(sumItens)}</p>
+     </li>
     `;
+}
+
+function filterVegetarian() {
+    const filterVegetarian = menuOptions.filter(product => product.vegan)
+    showAll(filterVegetarian);
 }
 
 
 buttonShowAll.addEventListener("click", () => showAll(menuOptions))
 buttonMapAll.addEventListener("click", mapAll)
 buttonSumAll.addEventListener("click", sumAllItens)
+buttonFilterAll.addEventListener("click", filterVegetarian)
